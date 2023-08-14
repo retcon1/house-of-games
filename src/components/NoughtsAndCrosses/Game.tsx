b1b6980f3fa7d2e0228443358f7799ad42f43b1d
retcon1 <benlucascoding@outlook.com>
2023-08-14 18:02:09 +0100

    feat: adds playing against computer option to N&C game

--- src/components/NoughtsAndCrosses/Game.tsx
+++ src/components/NoughtsAndCrosses/Game.tsx
@@ -1,28 +1,55 @@
 import { useState } from "react";
 import { Board } from "./Board";
+import { aiMove, calculateWinner } from "./util";
 
 export const Game = () => {
   const [board, setBoard] = useState<string[]>(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState<boolean>(true);
+  const [xScore, setXScore] = useState(0);
+  const [oScore, setOScore] = useState(0);
+  const [ai, setAi] = useState(false);
 
   const cellClick = (index: number) => {
     const newBoard = [...board];
 
-    // prevents duplicate clicks and checks for winner
+    // prevents duplicate clicks
     if (newBoard[index] || calculateWinner(newBoard)) {
       return;
     }
+
     newBoard[index] = xIsNext ? "X" : "O";
     setBoard(newBoard);
     setXIsNext(!xIsNext);
+
+    // utilised in AI mode
+    if (ai) {
+      const aiMoveIndex = aiMove(newBoard);
+      if (aiMoveIndex !== -1) {
+        newBoard[aiMoveIndex] = "O";
+        setBoard(newBoard);
+        setXIsNext(true);
+      }
+    }
   };
 
   const resetClick = () => {
+    if (winner === "O") {
+      setOScore(oScore + 1);
+    } else if (winner === "X") {
+      setXScore(xScore + 1);
+    }
     setBoard(Array(9).fill(null));
     setXIsNext(true);
   };
 
+  const aiClick = () => {
+    setXIsNext(true);
+    setBoard(Array(9).fill(null));
+    setAi(!ai);
+  };
+
   const winner = calculateWinner(board);
+
   const status = winner
     ? `Winner: ${winner}`
     : `Next player: ${xIsNext ? "X" : "O"}`;
@@ -30,35 +57,19 @@ export const Game = () => {
   return (
     <div className="noughts-and-crosses">
       <h1>Noughts & Crosses</h1>
+      {oScore || xScore ? (
+        <div className="scoreboard">
+          X Score: {xScore} <br /> O Score: {oScore}
+        </div>
+      ) : null}
       <div className="status">{status}</div>
       <Board board={board} onClick={cellClick} />
-      <button onClick={resetClick}>RESET</button>
+      <button onClick={resetClick} className="reset-btn">
+        RESET
+      </button>
+      <button onClick={aiClick} className="ai-btn">
+        Play against AI
+      </button>
     </div>
   );
 };
-
-function calculateWinner(board: string[]): string | null {
-  // Logic to check for a winner
-
-  const winPatterns = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (const [a, b, c] of winPatterns) {
-    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-      return board[a];
-    }
-  }
-
-  if (board.every((element) => element)) {
-    return "Draw!";
-  }
-  return null;
-}
